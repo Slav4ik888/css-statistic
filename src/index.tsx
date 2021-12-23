7cfada633f2853eea28bdc4a0910213477a3e38e
Vyacheslav <korzan.va@mail.ru>
2021-12-23 17:22:14 +0800

    [client] - merge v.2

--- src/index.tsx
+++ src/index.tsx
@@ -23,4 +23,4 @@ ReactDOM.render(
   </Provider>
   , document.getElementById('root'));
 
-// git add . && git commit -m "[client] - merge v.2" && git push origin update-to-react
+// git add . && git commit -m "[client] - merge v.2" && git push origin master
