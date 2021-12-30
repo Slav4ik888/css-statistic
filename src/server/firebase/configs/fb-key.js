35da2238ec02616761e19d19ae5a1d35ef192228
Vyacheslav <korzan.va@mail.ru>
2021-12-30 10:40:26 +0800

    [client] - remove config

--- src/server/firebase/configs/fb-key.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyCrbdoiX1MPP1KsYvZzIoNMVwA1CUzGuY0",
-  authDomain: "workspace-css.firebaseapp.com",
-  projectId: "workspace-css",
-  storageBucket: "workspace-css.appspot.com",
-  messagingSenderId: "466073260301",
-  appId: "1:466073260301:web:baca65b6184a9e13792044"
-};
